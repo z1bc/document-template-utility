2289c876a960a16e07e9befa56dbd8b27ccf4f69
Muhamed <muhamed.ramic@edu.fit.ba>
2020-06-19 01:01:17 +0200

    generated pdf using html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import Pdf from "./pdf/document.pdf";
 import json from './contact.json';
-import { PDFDocument } from "pdf-lib";
-
-const fetchPdf = () => {
-  return fetch(Pdf).then((pdf) => pdf.arrayBuffer());
-}
 
 const editPdf = async() => {
   const pdfjs = await import('pdfjs-dist/build/pdf');
@@ -35,7 +30,7 @@ const editPdf = async() => {
       });
       return trasnformedItem;
     });
-    console.log(page);
+    console.log(transformedValues);
 
     const canvas = document.getElementById("canvas");
     const viewport = page.getViewport({
@@ -46,19 +41,44 @@ const editPdf = async() => {
       canvasContext: canvas.getContext("2d"),
       viewport: viewport
     })
-    const show = await page.getTextContent();
     return data;
-  }).then((data) => {
-    console.log(data);
-  })
+  });
 };
 
- function App() {
-  editPdf();
+const printPdf = (e) => {
+  e.preventDefault();
+  const table = document.getElementById("main").innerHTML;
+  console.log(table);
+  const printWindow = window.open('','','height=400, width=600');
+  printWindow.document.write('<html><head><title>DIV Contents</title>');
+  printWindow.document.write('</head><body >');
+  printWindow.document.write(table);
+  printWindow.document.write('</body></html>');
+  printWindow.document.close();
+  printWindow.print();
+}
+
+function App() {
+  const data =  editPdf();
+  console.log(json.doc.photo);
   return (
     <div className="App">
-     <h1>nesto</h1>
-     <a href="./pdf/document.pdf" download="file">file</a>
+     <a onClick={printPdf} href="./pdf/document.pdf" download="file">Click here to generate new pdf</a>
+     <h1>Oldpdf</h1>
+     <div id="main" style={{display: "none"}}>
+      <img src="/logo.png" alt="pdf-logo"></img>
+      <table id="table" style={{borderTop: "1px solid black", borderRight: "1px solid black",borderLeft:"1px solid black", marginTop: "25px"}}>
+      <tbody>
+        {
+          Object.keys(json.doc).map(key => {
+            return <tr style={{border: "1px solid black"}}><td style={{width: "20px", paddingLeft: "5px", borderRight: "1px solid black", borderBottom: "1px solid black"}}>{key}</td><td style={{width: "400px", paddingLeft: "5px", borderBottom: "1px solid black"}}>
+              {key!=="photo"?json.doc[key]: <img src={json.doc[key]} alt="img"></img>}}
+              </td></tr>
+          })
+        }
+      </tbody>
+      </table>
+     </div>
      <canvas id="canvas" width="600" height="600"></canvas>
     </div>
   );
